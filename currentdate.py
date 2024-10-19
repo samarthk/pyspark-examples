7da07eac9e40cdee9f701993d1e2b48d39fdf88e
Samarth Kumar <samarthcse@gmail.com>
2024-10-19 11:22:43 +0200

    Update currentdate.py

--- currentdate.py
+++ currentdate.py
@@ -9,9 +9,11 @@ import pyspark
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
 from pyspark.sql.functions import to_timestamp, current_timestamp
+
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType
 
 spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
 
 schema = StructType([
             StructField("seq", StringType(), True)])
